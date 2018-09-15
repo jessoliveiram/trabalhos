c8d6c8381dc57078d640d905e1abbcfba8ba7105
jessica <jessmartins@poli.ufrj.br>
2018-09-15 12:58:18 -0300

    andamento do trabalho de estrutura de dados

---
+++ teste.c
@@ -0,0 +1,10 @@
+char *ptr;
+ptr = malloc (1);
+scanf ("%c", ptr);
+
+typedef struct {
+   int dia, mes, ano; 
+} data;
+data *d;
+d = malloc (sizeof (data));
+d->dia = 31; d->mes = 12; d->ano = 2016;
